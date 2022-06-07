26a2223710df77cef3e81d92a51c1ffbd2357390
Akezhan345 <93520292+Akezhan345@users.noreply.github.com>
2022-06-07 20:15:16 +0600

    Add files via upload

---
+++ module/someFunction.js
@@ -0,0 +1,7 @@
+// I tried to but something went wrong. Maybe because of windows
+// export default function confirm(name, amountOfCash, password){
+// 		myUser.fName = name;
+// 		myUser.password = password;
+// 		myUser.cashAmount = amountOfCash;
+// 	}
+
